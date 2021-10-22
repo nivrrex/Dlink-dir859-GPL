c21089c5d7193676d3f2d8c0101a5fe81ccfb893
nivrrex <nivrrex@gmail.com>
2021-10-22 13:16:00 +0800

    删除：     README.md 	删除：     arch.mk 	删除：     boards/wrgac37/.config 	删除：     boards/wrgac37/path.mk 	删除：     config.mk 	删除：     include/elbox_config.h 	删除：     kernel 	删除：     kernels/QCA956X-2.6.31-LSDK-10.2-00082-4/arch.mk 	删除：     kernels/QCA956X-2.6.31-LSDK-10.2-00082-4/path.mk 	删除：     make3.81 	删除：     path.mk 	删除：     progs.board 	删除：     progs.brand 	删除：     progs.gpl/arch.mk 	删除：     progs.priv/arch.mk 	删除：     progs.template 	删除：     templates/aries/dlink.2013gui/.config 	删除：     templates/aries/dlink.2013gui/arch.mk 	删除：     templates/aries/dlink.2013gui/path.mk 	删除：     templates/aries/progs/.config 	删除：     templates/aries/progs/arch.mk 	删除：     templates/aries/progs/path.mk

--- path.mk
+++
@@ -1,5 +0,0 @@
-# This file is generated automatically during setup environment.
-# Ths environment is setup for wrgac37_dlink.2013gui_dir859 .
-TOPDIR:=/home/xiang/dir859_redo
-KERNELDIR:=/home/xiang/dir859_redo/kernel
-TARGET:=/home/xiang/dir859_redo/rootfs
